df4588bc297064d40a2a69130c034d7c5736c6c7
Agus Suarya <agus.suarya@spinroam.com>
2020-06-03 20:35:09 +0800

    Foreign transaction model

---
+++ app/Models/ForeignTransaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ForeignTransaction extends Model
+{
+
+    const TYPE_VOLUME = 'volume';
+    const TYPE_VALUE = 'value';
+
+    protected $fillable = [
+        'transaction_date',
+        'stock_code',
+        'last_price',
+        'change_value',
+        'change_percentage',
+        'grid_fluctuation_ratio',
+        'frequency',
+        'volume',
+        'value',
+        'type',
+        'foreign_buy',
+        'foreign_sell',
+        'net_buy',
+        'net_sell',
+    ];
+}
